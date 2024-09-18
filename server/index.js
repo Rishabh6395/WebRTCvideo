773808c286ded5ade706c91bccca66a9972c4018
Rishabh <rishabhbhatt956@gmail.com>
2024-09-18 11:49:28 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const {Server} = require('socket.io')
+
+const io = new Server({
+    cors: true,
+})
+const app = express()
+
+app.use(bodyParser.json())
+
+const emailToSocketMapping = new Map()
+
+
+io.on('connection', (socket)=>{
+    console.log("New connection")
+    socket.on('join-room', (data)=>{
+        const {roomId, emailId} = data;
+        console.log("User", emailId, 'joined room', roomId)
+        emailToSocketMapping.set(emailId, socket.id)
+        socket.join(roomId)
+        socket.emit('joined-room', {roomId})
+        socket.broadcast.to(roomId).emit('user-joined', {emailId})
+    })
+} )
+
+app.listen(8000,()=> console.log('Http server running at PORT 8000'))
+io.listen(8001)
